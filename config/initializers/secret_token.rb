898c94ddf6759ee300db02d4d37a7cce64429300
OpenShift guest <jzhanglsw@gmail.com>
2014-03-24 11:14:08 -0400

    First time commit. Get hype

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fightdex::Application.config.secret_key_base = '3a17f2b6058392662bba776000038f1cb9a356d17a969cda0414f44c742a26afef50a9219db05174b4ca9f7074f75448ac992ab5e6bae7aba8001527ccc1c74e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fightdex::Application.config.session_store :cookie_store, key: '_fightdex_session'
